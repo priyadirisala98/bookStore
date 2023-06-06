da86d0082a75c2bb0c8322e0748b6251ac44eaaa
priyadirisala98 <priya.dirisala98@gmail.com>
2023-06-05 22:37:55 -0500

    First commit

---
+++ bookStore/src/main/java/com/bookStore/controller/BookController.java
@@ -0,0 +1,88 @@
+package com.bookStore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bookStore.entity.Book;
+import com.bookStore.entity.MyBookList;
+import com.bookStore.service.BookService;
+import com.bookStore.service.MyBookListService;
+
+import java.util.*;
+
+@Controller
+public class BookController {
+	
+	@Autowired
+	private BookService service;
+	
+	@Autowired
+	private MyBookListService myBookService;
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/book_register")
+	public String bookRegister() {
+		return "bookRegister";
+	}
+	
+	@GetMapping("/available_books")
+	public ModelAndView getAllBook() {
+		List<Book>list=service.getAllBook();
+//		ModelAndView m=new ModelAndView();
+//		m.setViewName("bookList");
+//		m.addObject("book",list);
+		return new ModelAndView("bookList","book",list);
+	}
+	
+	@PostMapping("/save")
+	public String addBook(@ModelAttribute Book b) {
+		service.save(b);
+		return "redirect:/available_books";
+	}
+	@GetMapping("/my_books")
+	public String getMyBooks(Model model)
+	{
+		List<MyBookList>list=myBookService.getAllMyBooks();
+		model.addAttribute("book",list);
+		return "myBooks";
+	}
+	@RequestMapping("/mylist/{id}")
+	public String getMyList(@PathVariable("id") int id) {
+		Book b=service.getBookById(id);
+		MyBookList mb=new MyBookList(b.getId(),b.getName(),b.getAuthor(),b.getPrice());
+		myBookService.saveMyBooks(mb);
+		return "redirect:/my_books";
+	}
+	
+	@RequestMapping("/editBook/{id}")
+	public String editBook(@PathVariable("id") int id,Model model) {
+		Book b=service.getBookById(id);
+		model.addAttribute("book",b);
+		return "bookEdit";
+	}
+	@RequestMapping("/deleteBook/{id}")
+	public String deleteBook(@PathVariable("id")int id) {
+		service.deleteById(id);
+		return "redirect:/available_books";
+	}
+	 @GetMapping("/search_books")
+	    public String add(Model model) {
+	     List<Book> listbook = service.listAll();
+	        model.addAttribute("book", new Book());
+	        return "searchBook";
+	    }
+	 @PostMapping("/search_books")
+     public String doSearchBook(@ModelAttribute("bookSearchFormData") Book formData, Model model) {
+            Book b = service.get(formData.getId());
+            model.addAttribute("book", b);
+            return "searchBook";            
+     }
+	
+}
